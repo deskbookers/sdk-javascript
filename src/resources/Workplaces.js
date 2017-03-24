03b6f713b2386e466837a0286048887eaf27e4b5
yosh <yoshtalwar@gmail.com>
2017-03-24 11:54:02 +0100

    refactor: use array for params, rename urgency method

--- src/resources/Workplaces.js
+++ src/resources/Workplaces.js
@@ -25,13 +25,24 @@ export default class Workplaces extends Resource {
     })
   }
 
-  urgencyData (workplaceId, what) {
-    return this.request({
-      method: 'GET',
-      path: `/${this.endpoint}/${workplaceId}/urgency`,
-      params: {
-        what
+  urgency (workplaceId, params) {
+    return new Promise(async (resolve, reject) => {
+      const isArray = Array.isArray(params)
+
+      // API expects params as []
+      if (!isArray && typeof params === 'object') {
+        params = [ params ]
+      } else if (!isArray) {
+        reject('Parameters must be an Object or Array')
       }
+
+      const urgency = await this.request({
+        method: 'GET',
+        path: `/${this.endpoint}/${workplaceId}/urgency`,
+        params: { params }
+      })
+
+      resolve(urgency)
     })
   }
 }
--- test/workplaces.js
+++ test/workplaces.js
@@ -10,25 +10,17 @@ const deskbookers = new Deskbookers({
   host: process.env.API_HOST
 })
 
-test('Urgency data', async t => {
+test('Urgency', async t => {
   const now = moment()
   const monthAgo = now.clone().subtract(31, 'days')
   const start = monthAgo.toISOString()
   const end = now.toISOString()
 
-  const what = {
-    bookings: { type: 'bookings', start, end },
-    visitors: { type: 'visitors', start, end },
-    visitorsCity: { type: 'visitors', city: 'amsterdam', start, end },
-  }
+  const urgency = await deskbookers.workplaces.urgency(17657, [
+    { type: 'bookings', start, end },
+    { type: 'visitors', start, end },
+    { type: 'visitors', city: 'amsterdam', start, end },
+  ])
 
-  const data = await deskbookers.workplaces.urgencyData(17657, what)
-
-  t.truthy(
-    data && typeof data === 'object',
-    'Expected the result to be an object'
-  )
-  for (let key in what) {
-    t.truthy(typeof data[key] === 'number', `Expected '${key}' to be a number`)
-  }
+  t.truthy(urgency.length)
 })
