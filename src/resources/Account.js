a5006504968b1816ac9cde21845c4952226660b6
Leon van der Veen <vdvleon@gmail.com>
2017-05-17 17:54:03 +0200

    feat: add setLanguage / setTimezone functins, rewrite tests to work with local api instances

--- src/resources/Account.js
+++ src/resources/Account.js
@@ -136,6 +136,26 @@ export default class Account extends Resource {
     }
   }
 
+  async setLanguage (language) {
+    return await this.request({
+      method: 'POST',
+      path: 'user/language',
+      params: {
+        lang: language
+      }
+    })
+  }
+
+  async setTimezone (timezone) {
+    return await this.request({
+      method: 'POST',
+      path: 'user/timezone',
+      params: {
+        timezone
+      }
+    })
+  }
+
   async contexts ({ ...params } = {}) {
     return await this.request({
       method: 'GET',
