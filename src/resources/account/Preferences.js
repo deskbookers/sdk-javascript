fd5c6faee6b579513493c4fe8733209182ceee62
yosh <yoshtalwar@gmail.com>
2017-05-22 16:39:11 +0100

    refactor: update methods to new method of storing preferences

--- src/resources/account/Preferences.js
+++ src/resources/account/Preferences.js
@@ -1,5 +1,17 @@
 import DeskbookersError from '../../DeskbookersError'
 import Resource from '../Resource'
+import { pickAll } from 'ramda'
+
+// Construct and return Map
+function constructMap (obj) {
+  const map = new Map()
+
+  for (const key in obj) {
+    map.set(key, obj[key])
+  }
+
+  return map
+}
 
 export default class Preferences extends Resource {
   constructor (api) {
@@ -7,45 +19,53 @@ export default class Preferences extends Resource {
     this.endpoint = 'user/preferences'
   }
 
+  async getCurrentPreferences () {
+    const res = await this.request({
+      method: 'GET',
+      path: this.endpoint
+    })
+
+    return JSON.parse(res)
+  }
+
+  /**
+   * Retrieve a single preference
+   */
   async retrieve (key) {
-    const value = await this.request({
-      method: 'GET',
-      path: this.endpoint,
-      params: {
-        keys: [key]
-      }
-    })
-
-    return value[0]
+    const prefs = await this.list([key])
+    return prefs.get(key) || null
   }
 
+  /**
+   * List all preferences, or a subset
+   */
   async list (...keys) {
-    const preferences = await this.request({
-      method: 'GET',
-      path: this.endpoint,
-      params: {
-        keys
-      }
-    })
+    const current = await this.getCurrentPreferences()
 
-    // Construct and return Map
-    const map = new Map()
-    keys.forEach((key, i) => map.set(key, preferences[i]))
-    return map
+    // Filter preferences based on desired keys
+    const prefs = keys.length ? pickAll(keys, current): current
+
+    return constructMap(prefs)
   }
 
+  /**
+   * Update preferences by key
+   */
   async update (preferences = {}) {
-    const prefs = await this.request({
+    const prefs = await this.getCurrentPreferences()
+
+    for (const key in preferences) {
+      prefs[key] = preferences[key]
+    }
+
+    const res = await this.request({
       method: 'POST',
       path: this.endpoint,
       params: {
-        preferences
+        preferences: prefs
       }
     })
 
-    // Construct and return Map
-    const map = new Map()
-    Object.keys(prefs).forEach(key => map.set(key, prefs[key]))
-    return map
+    return constructMap(JSON.parse(res))
   }
 }
