73a83ab7285bdb6ba049672a721a702e84a0b447
yosh <yoshtalwar@gmail.com>
2017-05-23 14:53:43 +0200

    fix: encode slashes sent to report

--- test/actions.js
+++ test/actions.js
@@ -4,20 +4,26 @@ import test from 'ava'
 import Deskbookers from '../src'
 dotenv.load()
 
-function client () {
-  return new Deskbookers({
+const {
+  LOGIN_EMAIL,
+  LOGIN_PASSWORD
+} = process.env
+
+async function client (login = false) {
+  const deskbookers = new Deskbookers({
     https: process.env.API_HTTPS === 'true',
     host: process.env.API_HOST
   })
+
+  if (login) {
+    await deskbookers.account.login(LOGIN_EMAIL, LOGIN_PASSWORD)
+  }
+
+  return deskbookers
 }
 
 test('Reporting a problem', async t => {
-  const deskbookers = client()
-
-  await deskbookers.account.login(
-    process.env.LOGIN_EMAIL,
-    process.env.LOGIN_PASSWORD
-  )
+  const deskbookers = await client(true)
 
   const res = await deskbookers.actions.report({
     message: 'This function aint working!',
